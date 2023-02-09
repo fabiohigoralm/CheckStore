5f87b37c28f69440dc0d4538094d914c4c5c3d31
Fabio Almeida <fabiohigoralm2@gmail.com>
2023-02-09 19:44:10 -0300

    fix/close-menu-mobile-clickOutside

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,6 +1,12 @@
 var button = document.getElementsByClassName("menu-icon")[0];
 var menu = document.getElementsByClassName("menu-mobile")[0];
-button.addEventListener("click", function handleClick() {
+button.addEventListener("click", function handleClick(event) {
   menu.classList.toggle("menu-open");
   button.classList.toggle("menu-icon-close");
+});
+document.addEventListener("click", function closeMenu(event) {
+  const isClickInside = menu.contains(event.target);
+  if (!isClickInside) {
+    menu.classList.remove("menu-open");
+  }
 });
