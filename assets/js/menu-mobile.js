fe7fc344b4b4158fc7d6c8f73dfd928ef030759c
Fabio Almeida <fabiohigoralm2@gmail.com>
2023-02-10 16:46:46 -0300

    fix/button-close-menu

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,5 +1,7 @@
 var button = document.getElementsByClassName("menu-icon")[0];
 var menu = document.getElementsByClassName("menu-mobile")[0];
+var item = document.getElementsByClassName("menu-item");
+console.log(item);
 button.addEventListener("click", function handleClick(event) {
   menu.classList.toggle("menu-open");
   button.classList.toggle("menu-icon-close");
@@ -8,5 +10,12 @@ document.addEventListener("click", function closeMenu(event) {
   const isClickInside = menu.contains(event.target);
   if (!isClickInside) {
     menu.classList.remove("menu-open");
+    button.classList.remove("menu-icon-close");
   }
+});
+[...item].forEach(function (element) {
+  element.addEventListener("click", function (event) {
+    menu.classList.remove("menu-open");
+    button.classList.remove("menu-icon-close");
+  });
 });
